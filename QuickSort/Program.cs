2b9c008baafbc58e98e7c623fa7f498fc61e0952
Chlunidia <chlunidiapk@gmail.com>
2022-10-21 16:51:42 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array
+        private int n;
+    }
+}
